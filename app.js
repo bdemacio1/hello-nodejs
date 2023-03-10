1451d7f1b8f954a2e1edc4018aed58f02e7d7f71
Bobby Demacio <bobby.demacio@ontario.ca>
2023-03-10 14:05:03 -0500

    express

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Node app started");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+console.log('Node app started');
+
+app.set('port', port);
+
+app.get('/', (req, res) => {
+    res.send('Home');
+});
+
+app.get('/json', (req, res) => {
+    let json = {};
+    json.data = 'my data';
+    res.send(json);
+});
+
+app.listen(app.get('port'));
